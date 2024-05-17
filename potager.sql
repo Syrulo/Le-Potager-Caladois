9d865c005a71509a7f731d0be787e8ea7f3c9a6d
Thomas <tborestel@gmail.com>
2024-05-17 07:21:33 +0200

    UPDATE potager.sql

--- potager.sql
+++ potager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 14 mai 2024 à 16:53
+-- Généré le : ven. 17 mai 2024 à 07:17
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -38,10 +38,8 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20240514121231', '2024-05-14 14:12:53', 53),
-('DoctrineMigrations\\Version20240514122916', '2024-05-14 14:29:27', 71),
-('DoctrineMigrations\\Version20240514123106', '2024-05-14 14:31:13', 58),
-('DoctrineMigrations\\Version20240514141446', '2024-05-14 16:15:39', 117);
+('DoctrineMigrations\\Version20240515142452', '2024-05-15 16:25:01', 119),
+('DoctrineMigrations\\Version20240515145713', '2024-05-15 16:57:22', 51);
 
 -- --------------------------------------------------------
 
@@ -62,33 +60,16 @@ CREATE TABLE `messenger_messages` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `role`
---
-
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `role`
---
-
-INSERT INTO `role` (`id`, `nom`) VALUES
-(1, 'ROLE_USER');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:json)' CHECK (json_valid(`roles`)),
   `password` varchar(255) NOT NULL,
   `is_active` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '(DC2Type:datetime_immutable)',
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `updated_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -96,8 +77,17 @@ CREATE TABLE `utilisateur` (
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `email`, `password`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 'lol@lol.lol', 'lol', 1, '2024-05-14 16:35:58', NULL);
+INSERT INTO `utilisateur` (`id`, `email`, `roles`, `password`, `is_active`, `created_at`, `updated_at`) VALUES
+(53, 'genevieve09@boulanger.fr', '[\"ROLE_USER\"]', '$2y$13$GdR1lfRbbsAedDtdUimiyeVTqs7AkVjPHBhLFHXU5VNQ7kbzZz8h2', 1, '2024-05-15 19:25:21', NULL),
+(54, 'guyot.guy@sfr.fr', '[\"ROLE_USER\"]', '$2y$13$uht0N0DaklmVc7g47hi4w.YIOxHSAldbOqSwhHhri1IHHdL7fGP1m', 1, '2024-05-15 19:25:21', NULL),
+(55, 'ebigot@laposte.net', '[\"ROLE_USER\"]', '$2y$13$1Tpp3mTsccaPYcoYQ6rBkuknB4qzUv.9fPI2JafngnxC2AsycWFw6', 1, '2024-05-15 19:25:22', NULL),
+(56, 'tlabbe@riou.org', '[\"ROLE_USER\"]', '$2y$13$uCNal8O6gQZ4Pth67dme8u2ElLHa9AvYO5w5ZlOYcmYIGvllAMYxG', 1, '2024-05-15 19:25:22', NULL),
+(57, 'benoit.boucher@orange.fr', '[\"ROLE_USER\"]', '$2y$13$RjW5z8hHEwFjspJ9TbLQUO66JZ782UWYy3mDZCd7TDvCF9tvoKuq2', 1, '2024-05-15 19:25:22', NULL),
+(58, 'ischmitt@coulon.com', '[\"ROLE_USER\"]', '$2y$13$Ju8LR9t662KzdeFcZHqbTuZsgMO4X8fopiD0JcPVdjQb8K669Tkcy', 1, '2024-05-15 19:25:23', NULL),
+(59, 'lcosta@jacques.com', '[\"ROLE_USER\"]', '$2y$13$HPeWl8GFjur2RhK1fRkKOeWUFt93o5RhB7fHtWX3YDMg0lA93e0vO', 1, '2024-05-15 19:25:23', NULL),
+(60, 'diane56@tele2.fr', '[\"ROLE_USER\"]', '$2y$13$i85WfVuXBw7qCTTlBCuxA.LuwyfsiFKz.JCZNpKaqDelu4GBYwhjy', 1, '2024-05-15 19:25:23', NULL),
+(61, 'blanc.margaret@regnier.fr', '[\"ROLE_USER\"]', '$2y$13$LlHPc5ci9fpyCUaCHq9wuuXy2Xmc7jdzD3DWCZDjvQIyZ09joPmHW', 1, '2024-05-15 19:25:24', NULL),
+(62, 'celine.martineau@gros.com', '[\"ROLE_USER\"]', '$2y$13$U0d9KqGeou3nQbJflhEpGuPJkvmBJPNmcb/vpRD/hnqsPF41Ie.f2', 1, '2024-05-15 19:25:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -107,32 +97,14 @@ INSERT INTO `utilisateur` (`id`, `email`, `password`, `is_active`, `created_at`,
 
 CREATE TABLE `utilisateur_details` (
   `id` int(11) NOT NULL,
-  `prenom` varchar(150) DEFAULT NULL,
-  `nom` varchar(150) DEFAULT NULL,
+  `utilisateur_id` int(11) NOT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `nom` varchar(50) DEFAULT NULL,
   `tel` varchar(10) DEFAULT NULL,
   `adresse` varchar(255) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '(DC2Type:datetime_immutable)',
-  `updated_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
-  `utilisateur_id` int(11) NOT NULL
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `updated_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur_role`
---
-
-CREATE TABLE `utilisateur_role` (
-  `utilisateur_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `utilisateur_role`
---
-
-INSERT INTO `utilisateur_role` (`utilisateur_id`, `role_id`) VALUES
-(1, 1);
 
 --
 -- Index pour les tables déchargées
@@ -154,17 +126,11 @@ ALTER TABLE `messenger_messages`
   ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
 
 --
--- Index pour la table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_1D1C63B3E7927C74` (`email`);
+  ADD UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`);
 
 --
 -- Index pour la table `utilisateur_details`
@@ -173,14 +139,6 @@ ALTER TABLE `utilisateur_details`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `UNIQ_82DFC18FB88E14F` (`utilisateur_id`),
   ADD UNIQUE KEY `UNIQ_82DFC18F037AB0F` (`tel`);
-
---
--- Index pour la table `utilisateur_role`
---
-ALTER TABLE `utilisateur_role`
-  ADD PRIMARY KEY (`utilisateur_id`,`role_id`),
-  ADD KEY `IDX_9EE8E650FB88E14F` (`utilisateur_id`),
-  ADD KEY `IDX_9EE8E650D60322AC` (`role_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -193,16 +151,10 @@ ALTER TABLE `messenger_messages`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur_details`
@@ -219,13 +171,6 @@ ALTER TABLE `utilisateur_details`
 --
 ALTER TABLE `utilisateur_details`
   ADD CONSTRAINT `FK_82DFC18FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
-
---
--- Contraintes pour la table `utilisateur_role`
---
-ALTER TABLE `utilisateur_role`
-  ADD CONSTRAINT `FK_9EE8E650D60322AC` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_9EE8E650FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
