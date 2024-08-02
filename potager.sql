c791af73edf9e23430d4f52bbdf69487591e2128
Thomas <tborestel@gmail.com>
2024-08-02 09:18:18 +0200

    UPDATE .sql

--- potager.sql
+++ potager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 25 juil. 2024 à 18:29
+-- Généré le : ven. 02 août 2024 à 09:16
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -63,7 +63,8 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20240628122919', '2024-06-28 14:29:27', 32);
+('DoctrineMigrations\\Version20240729132033', '2024-07-29 15:20:38', 27),
+('DoctrineMigrations\\Version20240729140356', '2024-07-29 16:04:00', 27);
 
 -- --------------------------------------------------------
 
@@ -89,27 +90,29 @@ CREATE TABLE `messenger_messages` (
 
 CREATE TABLE `producteur` (
   `id` int(11) NOT NULL,
-  `nom` varchar(180) NOT NULL,
+  `nom` varchar(180) DEFAULT NULL,
   `email` varchar(180) NOT NULL,
-  `tel` varchar(10) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  `ville` varchar(80) NOT NULL,
-  `code_postal` int(11) NOT NULL,
+  `tel` varchar(10) DEFAULT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `ville` varchar(80) DEFAULT NULL,
+  `code_postal` int(11) DEFAULT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `updated_at` datetime DEFAULT NULL,
   `slug` varchar(255) NOT NULL,
   `image_name` varchar(255) NOT NULL,
   `image_size` int(11) NOT NULL,
-  `description` longtext NOT NULL
+  `description` longtext DEFAULT NULL,
+  `utilisateur_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `producteur`
 --
 
-INSERT INTO `producteur` (`id`, `nom`, `email`, `tel`, `adresse`, `ville`, `code_postal`, `created_at`, `updated_at`, `slug`, `image_name`, `image_size`, `description`) VALUES
-(1, 'La Ferme Caladoise', 'lafermecaladoise@gmail.com', '0689562456', '8 rue du patio', 'Villefranche-sur-Saône', 69400, '2024-05-25 10:42:57', '2024-05-25 10:42:57', 'La-Ferme-Caladoise', 'ferme.webp', 0, 'En activité depuis 1970, La Ferme Caladoise est une entreprise familiale depuis trois générations. \r\nAyant à cœur de fournir des produit de qualité, les légumes et fruits sont produits sur place.'),
-(11, 'Légumes du jardin', 'legumesdujardin@gmail.com', '0625323636', '8 rue Copernic', 'Limas', 69400, '2024-06-24 15:54:20', '2024-06-28 10:06:35', 'Legumes-du-jardin', 'legumesdujardin-667e6f0b465c7942689864.jpg', 138866, 'Légumes du jardin, c\'est des années de passion et d\'amour de la nature.\r\nTous les produits de cette coopérative agricole à taille humaine, sont récoltés à la main dans la plus pur tradition française d\'antan.');
+INSERT INTO `producteur` (`id`, `nom`, `email`, `tel`, `adresse`, `ville`, `code_postal`, `created_at`, `updated_at`, `slug`, `image_name`, `image_size`, `description`, `utilisateur_id`) VALUES
+(16, 'La Ferme Caladoise', 'lafermecaladoise@gmail.com', '0689562456', '8 rue du patio', 'Villefranche-sur-Saône', 69400, '2024-07-29 16:35:21', '2024-07-30 13:46:21', 'La-Ferme-Caladoise', 'lafermecaladoise-66a8d28d63edf465381239.jpg', 54309, 'En activité depuis 1970, La Ferme Caladoise est une entreprise familiale depuis trois générations. Ayant à cœur de fournir des produit de qualité, les légumes et fruits sont produits sur place.', 20),
+(17, 'Légumes du jardin', 'legumesdujardin@gmail.com', '0625323636', '8 rue Copernic', 'Limas', 69400, '2024-07-30 13:52:02', '2024-07-30 13:52:03', 'Legumes-du-jardin', 'legumesdujardin-66a8d3e36e367262888792.jpg', 138866, 'Légumes du jardin, c\'est des années de passion et d\'amour de la nature. Tous les produits de cette coopérative agricole à taille humaine, sont récoltés à la main dans la plus pur tradition française d\'antan.', 21),
+(18, 'Fruits du verger', 'fruitsduverger@gmail.com', '0632353431', '16 chemin de la tarte aux fruits', 'Anse', 69009, '2024-07-30 14:08:45', '2024-07-30 14:08:45', 'Fruits-du-verger', 'fruitsduverger-66a8d7cdb3a4d259559686.jpg', 150729, 'Vous aimez les fruits ? Vous allez adorer les fruits de nos vergers. Cueillis à la main, conditionnés par nos soins et pressez pour vous fournir les boissons les plus fruitées qui soit ainsi que les des fruits à consommer tel quel, Les fruits du verger, c\'est la référence lyonnaise.', 22);
 
 -- --------------------------------------------------------
 
@@ -122,7 +125,7 @@ CREATE TABLE `produit` (
   `categorie_id` int(11) DEFAULT NULL,
   `producteur_id` int(11) DEFAULT NULL,
   `nom` varchar(50) NOT NULL,
-  `prix` int(11) NOT NULL,
+  `prix` double NOT NULL,
   `image_name` varchar(255) NOT NULL,
   `image_size` int(11) NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -136,14 +139,15 @@ CREATE TABLE `produit` (
 --
 
 INSERT INTO `produit` (`id`, `categorie_id`, `producteur_id`, `nom`, `prix`, `image_name`, `image_size`, `created_at`, `updated_at`, `slug`, `description`) VALUES
-(1, 2, 1, 'Tomate', 0, 'tomates-66702ca061bee264215891.png', 610533, '2024-05-25 10:43:56', '2024-06-17 14:31:28', 'Tomate', 'La tomate se consomme comme un légume-fruit, crue ou cuite. Riche en eau, en antioxydant et de fibres. C\'est aussi une source de vitamine C. Autres avantages, elle est peu calorique. Environ 6-7 pièces au kg.\r\n\r\nOrigine France'),
-(2, 1, 1, 'Pommes', 110, 'pommes-667c1b7ca7d93158738233.jpg', 152636, '2024-05-25 10:49:27', '2024-06-26 15:45:32', 'Pommes', 'La pomme est riche en pectine. C\'est aussi une source de vitamine C et d\'antioxydants. Elle stimule le système immunitaire. Environ 5-6 pour faire un kilo.\r\n\r\nOrigine France.'),
-(8, 2, 11, 'radis', 200, 'radis-667e918318ee0095511000.jpg', 46227, '2024-06-26 14:53:10', '2024-06-28 12:33:39', 'radis', 'c\'est un radis'),
-(9, 1, 1, 'Poires', 380, 'poires-667c1bee6c0e7703230029.webp', 80536, '2024-06-26 15:47:26', '2024-06-26 15:47:26', 'Poires', 'La poire est une excellente source de fibres alimentaires et aussi riche en antioxydant. La poire conférence a la chair fine, juteuse et fondante avec une saveur légèrement acidulée. Environ 7 poires pour faire un kilo.'),
-(10, 1, 11, 'Oranges', 200, 'orange-667c1d23a0b6a295804480.webp', 38354, '2024-06-26 15:52:35', '2024-06-26 15:52:35', 'Oranges', 'Orange aussi bonne en jus ou à la main. \r\nL\'orange est une source de fibres, de calcium et de magnésium. Mais elle est aussi riche en vitamine C et pauvre en calories. Elle stimule le système immunitaire.'),
-(11, 1, 11, 'Pêches plates', 145, 'peche-plate-667c2339067a3803164341.jpg', 221307, '2024-06-26 16:18:32', '2024-06-26 16:18:33', 'Peches-plates', 'Les pêches sont des fruits climatériques charnus, juteux et sucrés, avec une chair jaune, blanche, ou rouge (sanguine), une peau veloutée de couleur jaune ou orange plus ou moins lavée de rose-carmin à rose-saumon ou brune chez les sanguines, et un noyau dur, adhérent ou non.'),
-(12, 1, 1, 'Pêches', 150, 'peches-667c235d32aaa293313028.webp', 75334, '2024-06-26 16:19:09', '2024-06-26 16:19:09', 'Peches', 'Les pêches sont des fruits climatériques charnus, juteux et sucrés, avec une chair jaune, blanche, ou rouge (sanguine), une peau veloutée de couleur jaune ou orange plus ou moins lavée de rose-carmin à rose-saumon ou brune chez les sanguines, et un noyau dur, adhérent ou non.'),
-(13, 3, 11, 'Jus de pommes', 200, 'jusdepommes-667e71f776632875643308.webp', 22722, '2024-06-28 10:14:26', '2024-06-28 10:19:03', 'Jus-de-pommes', 'Jus de pommes 100% naturel.');
+(19, 2, 16, 'Salade', 1.3, 'salade-laitue-66a8bc2c21760109966123.webp', 102024, '2024-07-30 12:10:50', '2024-07-30 12:24:50', 'Salade', 'C\'est une salade'),
+(20, 1, 18, 'Pommes', 1.1, 'pommes-667c1b7ca7d93158738233-66a8d907d3b24905142283.jpg', 152636, '2024-07-30 14:13:59', '2024-07-30 14:13:59', 'Pommes', 'La pomme est riche en pectine. C\'est aussi une source de vitamine C et d\'antioxydants. Elle stimule le système immunitaire.\r\nEnviron 5-6 pour faire un kilo.\r\nOrigine France.'),
+(21, 3, 18, 'Jus de pommes', 2.6, 'jusdepommes-66a8db3ce41ad539735521.webp', 22722, '2024-07-30 14:23:24', '2024-07-30 14:23:24', 'Jus-de-pommes', 'Jus de pommes 100% naturel.'),
+(22, 1, 18, 'Pêches', 1.5, 'peches-66a8dca1725c9001510746.webp', 75334, '2024-07-30 14:29:21', '2024-07-30 14:30:44', 'Peches', 'Les pêches sont des fruits climatériques charnus, juteux et sucrés, avec une chair jaune, blanche, ou rouge (sanguine), une peau veloutée de couleur jaune ou orange plus ou moins lavée de rose-carmin à rose-saumon ou brune chez les sanguines, et un noyau dur, adhérent ou non.'),
+(23, 1, 18, 'Pêches plates', 1.45, 'peche-plate-66a8dcdc26db4884344842.jpg', 221307, '2024-07-30 14:30:20', '2024-07-30 14:30:20', 'Peches-plates', 'Les pêches sont des fruits climatériques charnus, juteux et sucrés, avec une chair jaune, blanche, ou rouge (sanguine), une peau veloutée de couleur jaune ou orange plus ou moins lavée de rose-carmin à rose-saumon ou brune chez les sanguines, et un noyau dur, adhérent ou non.'),
+(24, 2, 17, 'Tomates', 2, 'tomates-66a8dda26402d270371819.jpg', 25457, '2024-07-30 14:33:38', '2024-07-30 14:33:38', 'Tomates', 'La tomate se consomme comme un légume-fruit, crue ou cuite. Riche en eau, en antioxydant et de fibres. C\'est aussi une source de vitamine C. Autres avantages, elle est peu calorique.\r\nEnviron 6-7 pièces au kg.\r\nOrigine France'),
+(25, 2, 17, 'Radis', 2, 'radis-66a8e605283cb070219192.webp', 65708, '2024-07-30 15:09:25', '2024-07-30 15:09:25', 'Radis', 'Toutes les parties de la plante sont comestibles, bien que sa racine pivot soit plus populaire. La peau et la chair du radis peuvent être de différentes couleurs, dont la plus courante est le rouge. Certaines variétés peuvent être bicolores, roses, violettes, vertes, blanches ou noires.'),
+(26, 1, 16, 'Poires', 3.8, 'poires-66a8e7b95a4bd528303356.webp', 22600, '2024-07-30 15:16:41', '2024-07-30 15:16:41', 'Poires', 'La poire est une excellente source de fibres alimentaires et aussi riche en antioxydant. La poire conférence a la chair fine, juteuse et fondante avec une saveur légèrement acidulée.\r\nEnviron 7 poires pour faire un kilo.'),
+(27, 1, 16, 'Oranges', 2, 'orange-66a8eaec336ca963172789.jpg', 161931, '2024-07-30 15:30:20', '2024-07-30 15:30:20', 'Oranges', 'L\'orange est une source de fibres, de calcium et de magnésium. Mais elle est aussi riche en vitamine C et pauvre en calories. Elle stimule le système immunitaire.');
 
 -- --------------------------------------------------------
 
@@ -167,7 +171,11 @@ CREATE TABLE `utilisateur` (
 
 INSERT INTO `utilisateur` (`id`, `email`, `roles`, `password`, `is_active`, `created_at`, `updated_at`) VALUES
 (3, 'admin@admin.admin', '[\"ROLE_USER\",\"ROLE_ADMIN\"]', '$2y$13$A.lwV.2CmjhpqKbFMadPXODo7lpvVo5WsgooQPHObNG7GIHh7vNmq', 1, '2024-05-25 10:35:28', '2024-05-25 10:35:28'),
-(5, 'lol@lol.lol', '[\"ROLE_USER\"]', '$2y$13$icXYM/8frXK44yQVCRjIkuA6zCZNuq.gwlsd/KpcymNMY07jYUbP6', 1, '2024-06-02 10:08:14', '2024-06-02 10:08:14');
+(5, 'lol@lol.lol', '[\"ROLE_USER\"]', '$2y$13$icXYM/8frXK44yQVCRjIkuA6zCZNuq.gwlsd/KpcymNMY07jYUbP6', 1, '2024-06-02 10:08:14', '2024-06-02 10:08:14'),
+(20, 'lal@lal.lal', '[\"ROLE_PRODUCTEUR\"]', '$2y$13$.zHRc5mlk55wsX.e7S2s5OMaKKqGpRH5tn6dYwrEVsb7aUYdYZ5iC', 0, '2024-07-29 16:35:21', '2024-07-29 16:35:21'),
+(21, 'legumes@gmail.com', '[\"ROLE_PRODUCTEUR\"]', '$2y$13$4gWzgJI2zLPHM7KEznv2MeI2Au06QNYT6Fkj89Ji3zLFnmqy03pUS', 0, '2024-07-30 13:52:02', '2024-07-30 13:52:02'),
+(22, 'fruits@gmail.com', '[\"ROLE_PRODUCTEUR\"]', '$2y$13$QN4lIjrBh8IijUv2vO3tNe4wKIlCCXsEeTxooG7Dw32u.nBVz8ywu', 0, '2024-07-30 14:08:45', '2024-07-30 14:08:45'),
+(23, 'test@test.test', '[\"ROLE_USER\"]', '$2y$13$1Nn9faZpOZ2GTHMOSr2xjuqkEqmrR3EjmNMU9/lqSN03IeOlDjJDW', 1, '2024-07-30 14:48:11', '2024-07-30 14:48:11');
 
 -- --------------------------------------------------------
 
@@ -194,7 +202,8 @@ CREATE TABLE `utilisateur_details` (
 
 INSERT INTO `utilisateur_details` (`id`, `utilisateur_id`, `prenom`, `nom`, `tel`, `adresse`, `ville`, `code_postal`, `created_at`, `updated_at`) VALUES
 (1, 3, 'admin', 'admin', '0625586566', '6 zelkcjzejn', 'admin', 32625, '2024-05-25 10:35:28', '2024-05-25 10:35:28'),
-(2, 5, 'Jean-Michel', 'DeTel', '0526353625', '6 rue des platanes', 'Limas', 69400, '2024-06-02 10:08:14', '2024-06-02 10:08:14');
+(2, 5, 'Jean-Michel', 'DeTel', '0526353625', '6 rue des platanes', 'Limas', 69400, '2024-06-02 10:08:14', '2024-06-02 10:08:14'),
+(7, 23, 'test', 'test', '0635323632', 'test', 'test', 69632, '2024-07-30 14:48:11', '2024-07-30 14:48:11');
 
 --
 -- Index pour les tables déchargées
@@ -227,7 +236,8 @@ ALTER TABLE `messenger_messages`
 ALTER TABLE `producteur`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `UNIQ_7EDBEE10E7927C74` (`email`),
-  ADD UNIQUE KEY `UNIQ_7EDBEE10F037AB0F` (`tel`);
+  ADD UNIQUE KEY `UNIQ_7EDBEE10F037AB0F` (`tel`),
+  ADD UNIQUE KEY `UNIQ_7EDBEE10FB88E14F` (`utilisateur_id`);
 
 --
 -- Index pour la table `produit`
@@ -272,29 +282,35 @@ ALTER TABLE `messenger_messages`
 -- AUTO_INCREMENT pour la table `producteur`
 --
 ALTER TABLE `producteur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur_details`
 --
 ALTER TABLE `utilisateur_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `producteur`
+--
+ALTER TABLE `producteur`
+  ADD CONSTRAINT `FK_7EDBEE10FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
 
 --
 -- Contraintes pour la table `produit`
