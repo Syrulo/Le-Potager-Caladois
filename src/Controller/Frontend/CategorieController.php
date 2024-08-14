2125fc8c988ce433af4549e4224ea5b209a3a650
Thomas <tborestel@gmail.com>
2024-08-14 15:55:43 +0200

    UPDATE categorie controller

--- src/Controller/Frontend/CategorieController.php
+++ src/Controller/Frontend/CategorieController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Frontend;
 
-use App\Entity\Categorie;
+// use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,24 +11,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
 {
-    #[Route('/categorie/{slug}', name: 'app_categorie.details', methods: ['GET'])]
-    public function detailsCategorie(Categorie $categorie, ProduitRepository $repoProduit): Response
-    {
-        $produits = $repoProduit->findByCategorie($categorie->getId());
-        return $this->render('frontend/categorie/detailsCategorie.html.twig', [
-            'categorie' => $categorie,
-            'produits' => $produits
-        ]);
-    }
+    // #[Route('/categorie/{slug}', name: 'app_categorie.details', methods: ['GET'])]
+    // public function detailsCategorie(Categorie $categorie, ProduitRepository $repoProduit): Response
+    // {
+    //     $produits = $repoProduit->findByCategorie($categorie->getId());
+    //     return $this->render('frontend/categorie/detailsCategorie.html.twig', [
+    //         'categorie' => $categorie,
+    //         'produits' => $produits
+    //     ]);
+    // }
 
-    #[Route('/categorie', name: 'app_categorie', methods: ['GET'])]
-    public function listCategorie(CategorieRepository $repoCategorie): Response
-    {
-        $categories = $repoCategorie->findAll();
-        return $this->render('frontend/categorie/list.html.twig', [
-            'categories' => $categories
-        ]);
-    }
+    // #[Route('/categorie', name: 'app_categorie', methods: ['GET'])]
+    // public function listCategorie(CategorieRepository $repoCategorie): Response
+    // {
+    //     $categories = $repoCategorie->findAll();
+    //     return $this->render('frontend/categorie/list.html.twig', [
+    //         'categories' => $categories
+    //     ]);
+    // }
 
     #[Route('/categories/{slug}', name: 'app_categorie', methods: ['GET'])]
     public function listeCategorie(string $slug, ProduitRepository $repoProduit, CategorieRepository $repoCategorie): Response
