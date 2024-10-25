6ee4fe5f7ba15cb92980ca14becd647048519e4d
Thomas <tborestel@gmail.com>
2024-10-25 17:02:26 +0200

    UPDATE arborescence fichiers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Visitor;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,5 +32,54 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * Gère la connexion des visitors.
+     *
+     * @param AuthenticationUtils $authenticationUtils Utilitaire d'authentification pour obtenir le dernier nom d'visitor et les erreurs.
+     * @return Response Une réponse HTTP qui rend le template de connexion ou redirige vers la page d'accueil si l'visitor est déjà connecté.
+     */
+    #[Route('/connexion', name: 'app_security_login', methods: ['GET', 'POST'])]
+    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home_page');
+        }
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * Gère l'inscription des nouveaux visitors.
+     *
+     * @param Request $request La requête HTTP.
+     * @param EntityManagerInterface $manager Le gestionnaire d'entités pour gérer les opérations de base de données.
+     * @return Response Une réponse HTTP qui rend le template d'inscription ou redirige après l'inscription.
+     */
+    #[Route('/inscription', 'app_security_inscription', methods: ['GET', 'POST'])]
+    public function inscription(Request $request, EntityManagerInterface $manager) : Response
+    {
+
+        $visitor = new Visitor();
+        $form = $this->createForm(InscriptionType::class, $visitor);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($visitor);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre compte a bien été crée.'
+            );
+
+            return $this->redirectToRoute('app_security_login');
+        }    
+        
+        return $this->render('security/inscription.html.twig', [
+            'form' => $form
+        ]);
     }
 }
--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -7,6 +7,7 @@ use App\Entity\Visitor;
 use App\Entity\Producer;
 use App\Form\AdminEditType;
 use App\Form\NewProducerType;
+use App\Form\VisitorEditAsAdminType;
 use App\Repository\VisitorRepository;
 use App\Repository\ProducerRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -98,6 +99,97 @@ class VisitorController extends AbstractController
             'form' => $form,
             'title_heading' => 'Editez votre profil'
         ]);
+    }
+
+    /**
+     * Affiche une liste d'utilisateurs, à l'exception des producteurs.
+     *
+     * @param UtilisateurRepository $repoUtilisateur Le repository pour accéder aux données des utilisateurs.
+     * @return Response Une réponse HTTP qui rend le template backend/utilisateur/list.html.twig avec les utilisateurs.
+     */
+    #[Route('/admin/visitor/', name: 'app_admin_visitor', methods: ['GET'])]
+    public function listUtilisateur(VisitorRepository $visitorRepository): Response
+    {
+        return $this->render('backend/utilisateur/list.html.twig', [
+            'visitors' => $visitorRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * Édite un utilisateur existant.
+     *
+     * @param Utilisateur $utilisateur L'entité utilisateur à éditer.
+     * @param Request $request La requête HTTP.
+     * @param EntityManagerInterface $entityManager Le gestionnaire d'entités.
+     * @param Security $security Le service de sécurité pour vérifier les rôles et l'utilisateur connecté.
+     *
+     * @return Response La réponse HTTP.
+     */
+    #[Route('/admin/visitor/edition/{id}', name: 'app_admin_visitor_edit', methods: ['GET', 'POST'])]
+    public function edit(Visitor $visitor, Request $request, EntityManagerInterface $entityManager, Security $security): Response
+    {
+        $form = $this->createForm(VisitorEditAsAdminType::class, $visitor);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupère les rôles de l'utilisateur
+            $role = $form->get('roles')->getData();
+
+            // Vérifie si l'utilisateur actuellement connecté est le même que l'utilisateur que nous essayons de modifier
+            if ($security->getUser() !== $visitor || $role !== 'ROLE_ADMIN' || in_array('ROLE_SUPER_ADMIN', $security->getUser()->getRoles())) {
+                // Ajoute le role à l'utilisateur connecté, fusionne et supprime les doublons
+                if ($role === 'ROLE_ADMIN') {
+                    $visitor->setRoles(array_unique(array_merge($visitor->getRoles(), ['ROLE_ADMIN'])));
+                } elseif ($role === 'ROLE_PRODUCTEUR') {
+                    $visitor->setRoles(array_unique(array_merge($visitor->getRoles(), ['ROLE_PRODUCTEUR'])));
+                } else {
+                    // Supprime le role ROLE_ADMIN et ROLE_PRODUCTEUR
+                    $roles = $visitor->getRoles();
+                    $roles = array_diff($roles, ['ROLE_ADMIN', 'ROLE_PRODUCTEUR']);
+                    $roles = array_values($roles); // Réindexez le tableau
+                    $visitor->setRoles($roles);
+                }
+            }
+
+            $entityManager->persist($visitor);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a bien été modifié');
+            return $this->redirectToRoute('app_admin_visitor', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backend/utilisateur/edit.html.twig', [
+            'utilisateur' => $visitor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/visitor/validate/{id}', name: 'app_validate_visitor')]
+    public function validateVisitor(Visitor $visitor, EntityManagerInterface $em): Response
+    {
+        $visitor->setStatus("confirmed");
+        $em->flush();
+        return $this->redirectToRoute('app_admin_visitor');
+    }
+
+    /**
+     * Supprime un utilisateur spécifique.
+     *
+     * @param Request $request La requête HTTP.
+     * @param Utilisateur $utilisateur L'entité utilisateur à supprimer.
+     * @param EntityManagerInterface $entityManager Le gestionnaire d'entités.
+     * @return Response Une réponse HTTP qui redirige vers la liste des utilisateurs après suppression.
+     */
+    #[Route('/admin/visitor/{id}', name: 'app_admin_visitor_delete', methods: ['POST'])]
+    public function deleteVisitor(Request $request, Visitor $visitor, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $visitor->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($visitor);
+            $entityManager->flush();
+
+        $this->addFlash('success', 'L\'utilisateur a bien été supprimé');
+
+        return $this->redirectToRoute('app_admin_visitor', [], Response::HTTP_SEE_OTHER);
+        }
     }
     
 }
