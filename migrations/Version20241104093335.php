5f36190195b394a6e7366eff0581d84fe94719ba
Thomas <tborestel@gmail.com>
2024-11-05 10:29:36 +0100

    UPDATE migration IF NOT EXISTS

--- migrations/Version20241104093335.php
+++ migrations/Version20241104093335.php
@@ -21,7 +21,7 @@ final class Version20241104093335 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE producer DROP INDEX IDX_976449DC70BEE6D, ADD UNIQUE INDEX UNIQ_976449DC70BEE6D (visitor_id)');
-        $this->addSql('ALTER TABLE producer CHANGE visitor_id visitor_id INT NULL');
+        $this->addSql('ALTER TABLE producer CHANGE visitor_id visitor_id INT NOT NULL');
     }
 
     public function down(Schema $schema): void
